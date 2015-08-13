580d987cdeda1297e9f4e329a56209f73f77fd78
Justin Varghese <Cloudz@Justins-MacBook-Air.local>
2015-08-13 16:56:37 -0600

    feat: add movement update to Enemy class's update function

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + dt*this.speed;
 }
 
 // Draw the enemy on the screen, required method for game
